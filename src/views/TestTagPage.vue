47aca848632a96ab1a93dbf2bd16090024d9c511
mardev 2 <mardev2@scg.com>
2021-01-14 15:48:37 +0700

    Add Visual

--- src/views/TestTagPage.vue
+++ src/views/TestTagPage.vue
@@ -20,15 +20,79 @@
                     <b-col cols="3">
                         <label class="float-right mr-4">Refresh:{{ ReadTime-RefeshTime }}</label>
                     </b-col>
-                    <b-col cols="1">
-                        <b-button class="float-right mr-2" size="sm" variant="primary" @click="AddTag">Add</b-button>
+                    <b-col cols="4">
+                        <b-button class="float-right mr-2" size="sm" variant="warning" @click="ShowVisual()"><b-icon-cursor></b-icon-cursor> Visual</b-button>
+                        <b-button class="float-right mr-2" size="sm" variant="primary" @click="AddTag"><b-icon-tag></b-icon-tag> Add</b-button>                        
                     </b-col>
                 </b-row>
             </b-col>
         </b-row>
+
+        <canvas id="SimCV" v-bind:style="{position: 'absolute', left:'500px', top: 70, 'z-index': 0}" ></canvas>
+
+        <b-row v-if="VisualOpen == true">
+            <b-col>
+                <!-- <canvas id="SimCV" v-bind:style="{position: 'absolute', left:50, top: 50, 'z-index': 0}" ></canvas> -->
+            </b-col>
+            <b-col cols="2">
+                <b-row>
+                    <b-col></b-col>
+                    <b-col><strong>X</strong></b-col>
+                    <b-col><strong>Y</strong></b-col>
+                </b-row>
+                <b-row>
+                    <b-col><label class="float-right text-success">Center</label></b-col>
+                    <b-col>{{CenterX}}</b-col>
+                    <b-col>{{CenterY}}</b-col>
+                </b-row>
+            </b-col>
+            <b-col cols="3">
+                <b-input-group class="mt-1 pr-2">
+                    <template #prepend>
+                        <b-input-group-text><strong>X:{{Visual[0].X}} Y:{{Visual[0].Y}}</strong></b-input-group-text>
+                    </template>
+                    <b-form-select v-model="Visual[0].Tag" :options="optTag"></b-form-select>
+                    <template #append>
+                        <b-input-group-text><strong>A</strong></b-input-group-text>
+                    </template>                    
+                </b-input-group>
+
+                <b-input-group class="mt-1 pr-2">
+                    <template #prepend>
+                        <b-input-group-text><strong>X:{{Visual[1].X}} Y:{{Visual[1].Y}}</strong></b-input-group-text>
+                    </template>
+                    <b-form-select v-model="Visual[1].Tag" :options="optTag"></b-form-select>
+                    <!-- <b-form-input v-model="Visual[1].Tag"></b-form-input> -->
+                    <template #append>
+                        <b-input-group-text><strong>B</strong></b-input-group-text>
+                    </template>                    
+                </b-input-group>
+
+                <b-input-group class="mt-1 pr-2">
+                    <template #prepend>
+                        <b-input-group-text><strong>X:{{Visual[2].X}} Y:{{Visual[2].Y}}</strong></b-input-group-text>
+                    </template>
+                    <b-form-select v-model="Visual[2].Tag" :options="optTag"></b-form-select>
+                    <template #append>
+                        <b-input-group-text><strong>C</strong></b-input-group-text>
+                    </template>                    
+                </b-input-group>
+
+                <b-input-group class="mt-1 pr-2">
+                    <template #prepend>
+                        <b-input-group-text><strong>X:{{Visual[3].X}} Y:{{Visual[3].Y}}</strong></b-input-group-text>
+                    </template>
+                    <b-form-select v-model="Visual[3].Tag" :options="optTag"></b-form-select>
+                    <template #append>
+                        <b-input-group-text><strong>D</strong></b-input-group-text>
+                    </template>                    
+                </b-input-group>
+            </b-col>
+        </b-row>
+
          <b-row>
             <b-col cols="6" v-for="(tag, index) in TagList" :key="index">
-                <b-card no-body class="mt-2 ml-2 mr-2 mb-2" @click="UpdateTag(tag)">
+                <b-card no-body class="mt-2 ml-2 mr-2 mb-2">
                     <b-card-body>
                         <b-row>
                             <b-col class="text-right" cols="2"></b-col>
@@ -47,8 +111,8 @@
                         </b-row>
                         <b-row>
                             <b-col class="text-right" cols="2">Target:</b-col>
-                            <b-col class="text-center"><b-form-input v-model="tag.target[0]" type="number"></b-form-input></b-col>
-                            <b-col class="text-center"><b-form-input v-model="tag.target[1]" type="number"></b-form-input></b-col>
+                            <b-col class="text-center"><b-form-input v-model.number="tag.target[0]" type="number"></b-form-input></b-col>
+                            <b-col class="text-center"><b-form-input v-model.number="tag.target[1]" type="number"></b-form-input></b-col>
                         </b-row>
                         <b-row class="mt-1">
                             <b-col class="text-right" cols="2">Detected:</b-col>
@@ -102,7 +166,18 @@ export default {
             ],
             CountDownRefresh:0,
             RefeshTime:0,
-            ReadTime:1
+            ReadTime:1,
+            VisualOpen:false,
+            Visual:[
+                {Tag:"", X:0, Y:0},
+                {Tag:"", X:0, Y:0},
+                {Tag:"", X:0, Y:0},
+                {Tag:"", X:0, Y:0}
+            ],
+            CenterX:0,
+            CenterY:0,
+            optTag:[],
+            SimCV:{}
         }
     },
     methods:{
@@ -131,6 +206,7 @@ export default {
         UpdateTag(Tag){
             this.UpdateDiffPostion(Tag);
             this.UpdateDatefromStamp(Tag);
+            this.UpdateVisualTag(Tag);
         },
         GetTagsPosition(){
             if(this.TagList.length == 0)
@@ -152,6 +228,7 @@ export default {
                     }
                 })
                 .catch(err => {
+                    // this.UpdateVisualTag(Tag); //TestOnly
                     window.console.log(err);
                 })
         },
@@ -178,7 +255,59 @@ export default {
                 diff:[0,0,0]
             }
             this.TagList.push(NewTag);
+        },
+        ShowVisual(){
+            this.VisualOpen = true;
+            this.optTag = [];
+            let opt = { text: "--Select--", value: "" };
+            this.optTag.push(opt);
+            for(let i = 0; i < this.TagList.length; i++){
+                opt = { text: this.TagList[i].name, value: this.TagList[i].id };
+                this.optTag.push(opt);
+            }
+        },
+        UpdateVisualTag(Tag){
+            let VTag = this.Visual.filter(x => x.Tag == Tag.id);
+            // console.log(VTag);
+            if(VTag.length > 0){
+                VTag.X = Tag.position[0];
+                VTag.Y = Tag.position[1];
+                // VTag[0].X = Tag.target[0];
+                // VTag[0].Y = Tag.target[1];
+            }
+            this.CalculateCenter();
+        },
+        CalculateCenter(){
+            let SumX = 0;
+            let SumY = 0;
+            for(let i = 0; i < this.Visual.length; i++){
+                SumX += this.Visual[i].X;
+                SumY += this.Visual[i].Y;
+            }
+            let CX = SumX/this.Visual.length;
+            let CY = SumY/this.Visual.length;
+            this.CenterX = CX;
+            this.CenterY = CY;
+        },
+        DrawVisual(){
+            let c = document.getElementById("SimCV");
+            c.width = 200;
+            c.height = 200;  
+            let ctx = c.getContext("2d");    
+            this.SimCV = ctx;
+
+            let X = 0;
+            let Y = 0;
+
+            this.SimCV.beginPath();
+            this.SimCV.lineWidth = "0.5";
+            this.SimCV.strokeStyle = "#F9B223";
+            this.SimCV.rect(0, 20, 100, 100);
+            this.SimCV.stroke();
         }
+    },
+    mounted(){
+        
     },
     timers: {
         AutoRefresh: { time: 1000, autostart: true, repeat:true }
